d9172c353c12cd5425298504fd7e8dac998926c5
Edward Boland <mr.eboland@gmail.com>
2021-01-18 19:29:17 -0500

    if statements

---
+++ toppings.py
@@ -0,0 +1,6 @@
+# To check for an inequality, we use ! =. The "!" stands for not, so not equal
+requestedTopping = "mushrooms"
+
+# The below reads, if the var requestedTopping does not equal anchovies, which is true, then execute the next line. reqTopping is not equal to anchovies, is true, execute code. 
+if requestedTopping != "anchovies":
+    print("Hold the anchovies!")
